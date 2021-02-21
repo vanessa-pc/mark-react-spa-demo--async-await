30bce0578c1b44c33eeb9c57637156d1b30c570e
Richard Ng <richardcrng@gmail.com>
2021-02-21 15:04:46 +0000

    Create async return demo

---
+++ src/1-async-return.ts
@@ -0,0 +1,12 @@
+function greet(): void {
+  console.log("hello world"!);
+}
+
+async function promiseToGreet(): Promise<void> {
+  console.log("hello world"!);
+}
+
+greet();
+// promiseToGreet()
+// greet().then(() => console.log("CANNOT do this"));
+// promiseToGreet().then(() => console.log("CAN do this"));
